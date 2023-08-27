d82022ca5a653321d1e7e686e14d5836500eb88b
aliasghar138006 <101926720+aliasghar138006@users.noreply.github.com>
2023-08-27 17:19:43 +0330

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -15,11 +15,5 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 # application = get_wsgi_application()
 
-# vercel_app/wsgi.py
-# app = get_wsgi_application()
-
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
-application = get_wsgi_application()
 app = application
+
